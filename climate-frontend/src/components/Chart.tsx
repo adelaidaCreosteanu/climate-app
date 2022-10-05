a1894a1c351741cc8c42b8c8e5b585ea507c3c48
Heidi <adelaida.creosteanu@gmail.com>
2022-10-05 20:15:10 +0300

    chore: format with prettier

--- climate-frontend/src/components/Chart.tsx
+++ climate-frontend/src/components/Chart.tsx
@@ -1,56 +1,62 @@
 import {
-    LineChart,
-    Line,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip,
-    Legend
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
 } from "recharts";
 
 interface IChartProps {
-    subsector: IYearlyEmissions[] | undefined
+  subsector: IYearlyEmissions[] | undefined;
 }
 
 export default function EmissionsChart(props: IChartProps) {
-    if (props.subsector && props.subsector?.length > 0) {
-        const country_set = Object.keys(props.subsector[0]).filter(x => x !== "year");
-        const lineColors = ["#76b7e6", "#ff7f0e", "#cc0000", "#cd5f96", "#6aa84f", "#16537e", "#906e04"]
+  if (props.subsector && props.subsector?.length > 0) {
+    const country_set = Object.keys(props.subsector[0]).filter(
+      (x) => x !== "year"
+    );
+    const lineColors = [
+      "#76b7e6",
+      "#ff7f0e",
+      "#cc0000",
+      "#cd5f96",
+      "#6aa84f",
+      "#16537e",
+      "#906e04",
+    ];
 
-        return (
-            <LineChart
-                width={700}
-                height={400}
-                data={props.subsector}
-                margin={{
-                    top: 30,
-                    right: 30,
-                    left: 30,
-                    bottom: 10
-                }}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="year" />
-                <YAxis label={{ value: "M Tonnes CO2E", angle: -90, position: "left" }} />
-                <Tooltip />
-                <Legend />
-                {country_set.map((
-                    country_code, country_index
-                ) =>
-                    <Line
-                        type="monotone"
-                        dataKey={country_code}
-                        stroke={lineColors[country_index % lineColors.length]}
-                        activeDot={{ r: 8 }}
-                    />
-
-                )}
-
-            </LineChart>
-        );
-    } else {
-        return (
-            <div> No data to plot! </div>
-        );
-    }
+    return (
+      <LineChart
+        width={700}
+        height={400}
+        data={props.subsector}
+        margin={{
+          top: 30,
+          right: 30,
+          left: 30,
+          bottom: 10,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="year" />
+        <YAxis
+          label={{ value: "M Tonnes CO2E", angle: -90, position: "left" }}
+        />
+        <Tooltip />
+        <Legend />
+        {country_set.map((country_code, country_index) => (
+          <Line
+            type="monotone"
+            dataKey={country_code}
+            stroke={lineColors[country_index % lineColors.length]}
+            activeDot={{ r: 8 }}
+          />
+        ))}
+      </LineChart>
+    );
+  } else {
+    return <div> No data to plot! </div>;
+  }
 }
